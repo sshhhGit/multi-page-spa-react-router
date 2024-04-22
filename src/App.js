a545e026e2dbacb92da334555aea89e79b97b4a3
sshhhgit <seungho0497@hanmail.net>
2024-04-23 08:45:56 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Product";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage />},
+  { path: '/products', element: <ProductsPage />}
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
+function App() {
+  return <RouterProvider router={router}/>;
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+      <>
+            <h1>My Home Page</h1>
+            <p>Go to <a href="/products">the list of products</a></p>
+      </>
+    );
+}
+
+export default HomePage;
