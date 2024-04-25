5d41e15f1d0ab890e94204bfb7778c9ab883a263
sshhhgit <seungho0497@hanmail.net>
2024-04-25 09:01:00 +0900

    -

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Product";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -14,9 +16,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
